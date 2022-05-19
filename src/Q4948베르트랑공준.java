84a2401a562a83637fe40668ed3445e0077fb7b7
jemoo <wpan1060@naver.com>
2022-05-19 19:27:26 +0900

    4948번 베르트랑공준 추가

---
+++ src/Q4948베르트랑공준.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q4948베르트랑공준 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true){
+
+            int a = Integer.parseInt(br.readLine());
+
+            if(a == 0)
+                break;
+
+            int count = 0;
+
+
+            for(int i = a + 1; i <= 2*a; i++){
+
+                int check = 0;
+
+
+                for(int j = 2; j <=(int)Math.sqrt(i); j++){
+
+                    if(i % j == 0){
+                        check++;
+                        break;
+                    }
+                }
+
+                if(check == 0){
+                    count++;
+                }
+
+            }
+
+            System.out.println(count);
+        }
+
+    }
+}
