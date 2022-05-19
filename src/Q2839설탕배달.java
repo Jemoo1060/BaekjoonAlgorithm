8bea05d707e23af078b60738a6a41f752a1974b4
jemoo <wpan1060@naver.com>
2022-05-19 19:24:02 +0900

    2839번 설탕배달 추가

---
+++ src/Q2839설탕배달.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Q2839설탕배달 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int count = 0;
+
+        for(int i = a/5; i >= 0; i--){
+            int b = a - 5*i;
+
+            if(b%3 == 0){
+                int c = b/3;
+                count = i + c;
+                System.out.println(count);
+                break;
+            }
+
+            if(i == 0 && b%3!=0){
+                System.out.println(-1);
+            }
+        }
+    }
+}
