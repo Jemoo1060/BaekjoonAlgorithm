91086ce070a3473e26b411a20600c5cc3ebf8e7c
jemoo <wpan1060@naver.com>
2022-05-19 20:46:29 +0900

    10250번 ACM호텔

---
+++ src/Q10250ACM호텔.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q10250ACM호텔 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < a; i++){
+            String input = br.readLine();
+
+            String[] split = input.split(" ");
+
+            int H = Integer.parseInt(split[0]);
+            int A = Integer.parseInt(split[2]);
+
+
+            int b = A/H + 1;
+            int c = A%H;
+
+            if( c == 0){
+                c = H;
+                b = A/H;
+            }
+            
+            System.out.println(c*100 + b);
+        }
+
+
+    }
+}
