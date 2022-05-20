9e30460b850835458780f1a18f80fbba994cd726
jemoo <wpan1060@naver.com>
2022-05-20 13:53:51 +0900

    1110번 더하기 사이클 추가

---
+++ src/Q1110더하기사이클.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q1110더하기사이클 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int check = a;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        int count = 0;
+        while (true){
+            b = a%10;
+            c = (a/10 + a%10)%10;
+            d = b*10 + c;
+
+            count++;
+            if(check == d){
+                break;
+            }
+
+            a = d;
+        }
+
+        System.out.println(count);
+
+
+
+    }
+}
