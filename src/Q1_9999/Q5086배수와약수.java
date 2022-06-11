0538b3bd05fe7cd99d68b5f915d15c0d7b491b96
jemoo <wpan1060@naver.com>
2022-06-11 21:37:36 +0900

    5086번 배수와 약수 추가

---
+++ src/Q1_9999/Q5086배수와약수.java
@@ -0,0 +1,40 @@
+package Q1_9999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q5086배수와약수 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+
+            if(a == 0 && b == 0)
+                break;
+
+
+            if(b % a == 0) {
+                sb.append("factor\n");
+            }
+            else if(a % b == 0) {
+                sb.append("multiple\n");
+            }
+            else {
+                sb.append("neither\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+    
+}
