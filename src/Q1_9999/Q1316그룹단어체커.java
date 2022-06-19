d4e30c4d4ced5a0528dff1073c710ab3c2fce28d
jemoo <wpan1060@naver.com>
2022-06-19 22:07:28 +0900

    1316번 그룹 단어 체커 추가

---
+++ src/Q1_9999/Q1316그룹단어체커.java
@@ -0,0 +1,39 @@
+package Q1_9999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q1316그룹단어체커 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            String S = br.readLine();
+            boolean check[] = new boolean[26];
+            boolean tmp = true;
+
+            for (int j = 0; j < S.length(); j++) {
+                int index = S.charAt(j)-'a';
+                if(check[index]) {
+                    if(S.charAt(j) != S.charAt(j-1)) {
+                        tmp = false;
+                        break;
+                    }
+                }else {
+                    check[index] = true;
+                }
+            }
+            if(tmp) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+    
+}
