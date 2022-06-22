a1d23df4d08eb08773c95234b07976feba706843
jemoo <wpan1060@naver.com>
2022-06-22 09:20:46 +0900

    4673번 셀프넘버 추가

---
+++ src/Q1_9999/Q4673셀프넘버.java
@@ -0,0 +1,40 @@
+package Q1_9999;
+
+public class Q4673셀프넘버 {
+
+    public static void main(String[] args) {
+
+        boolean[] check = new boolean[10001];
+
+        for (int i = 1; i < 10001; i++){
+            int n = d(i);
+
+            if(n < 10001){
+                check[n] = true;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < 10001; i++) {
+            if (!check[i]) {
+                sb.append(i).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+
+
+
+    public static int d(int number){
+        int sum = number;
+
+        while(number != 0){
+            sum = sum + (number % 10);
+            number = number/10;
+        }
+
+        return sum;
+    }
+
+}
