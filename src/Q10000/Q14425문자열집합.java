db65ab4faa16c14d5d10eeac45f974dcf7dab0ab
jemoo <wpan1060@naver.com>
2022-07-06 22:06:14 +0900

    14425번 문자열 집합 추가

--- src/Q10000/Q14425문자열집합.java
+++ src/Q10000/Q14425문자열집합.java
@@ -1,2 +1,38 @@
-package Q10000;public class Q14425문자열집합 {
+package Q10000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Q14425문자열집합 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer input = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(input.nextToken());
+        int M = Integer.parseInt(input.nextToken());
+
+        int count = 0;
+
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        for(int i = 0; i < N; i++){
+            hm.put(br.readLine(),1);
+        }
+
+        for(int i = 0; i < M; i++){
+            String check  = br.readLine();
+            if(hm.get(check) != null){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+
 }
