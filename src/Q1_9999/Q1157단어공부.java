1d101fe13740206c94186e26ae857035f78c3afc
jemoo <wpan1060@naver.com>
2022-10-19 22:44:40 +0900

    1157번  단어공부 추가

---
+++ src/Q1_9999/Q1157단어공부.java
@@ -0,0 +1,32 @@
+package Q1_9999;
+
+import java.io.IOException;
+
+public class Q1157단어공부 {
+
+    public static void main(String[] args) throws  IOException {
+
+        int[] arr = new int[26];
+        int c = System.in.read();
+        while (c > 64) {
+            if (c < 91) {
+                arr[c - 65]++;
+            } else {
+                arr[c - 97]++;
+            }
+            c = System.in.read();
+        }
+        int max = -1;
+        int ch = -2;
+        for (int i = 0; i < 26; i++) {
+
+            if (arr[i] > max) {
+                max = arr[i];
+                ch = i;
+            } else if (arr[i] == max) {
+                ch = -2;
+            }
+        }
+        System.out.print((char) (ch+65));
+    }
+}
